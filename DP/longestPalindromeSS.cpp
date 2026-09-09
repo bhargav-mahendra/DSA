322fd94103d763cc48c977ccdcba0f61f51fdaa9
Bhargav-mahendra <bhargavanugandhula1003@gmail.com>
2026-09-09 11:48:54 +0530

    Added longest palindromic subsequence solution

---
+++ DP/longestPalindromeSS.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int longestPalindromeSubseq(string &s, string &t) {
+        int n = s.size();
+        int m = t.size();
+        vector<int> prev(m+1), curr(m+1);
+        for(int j = 0;j<=m;j++) prev[j] = 0;
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1;j<=m;j++)
+            {
+                if(s[i-1] == t[j-1]){
+                    curr[j] = 1 + prev[j-1];
+                }
+                else{
+                    curr[j] = max(curr[j-1], prev[j]);
+                }
+            }
+            prev = curr;
+        }
+        return prev[m];
+    }   
+int main()
+{
+    string s = "bbabcbcab";
+    string t = s; 
+
+    reverse(t.begin(), t.end());
+    cout<<longestPalindromeSubseq(s, t);
+}
