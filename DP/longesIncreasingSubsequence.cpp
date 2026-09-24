dd1d8e966ea4108c539f8e3d408494e060c684e7
Bhargav-mahendra <bhargavanugandhula1003@gmail.com>
2026-09-24 14:34:46 +0530

    Added Longest Increasing Subsequence problem

--- DP/longesIncreasingSubsequence.cpp
+++ DP/longesIncreasingSubsequence.cpp
@@ -93,6 +93,26 @@ int solveSp(vector<int>&nums)
     }
     return maxi;
 }
+
+int solveBS(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> temp;
+    int len = 1;
+    temp.push_back(nums[0]);
+    for(int i = 1; i<n;i++)
+    {
+        if(nums[i] > temp.back()){
+            temp.push_back(nums[i]);
+            len++;
+        }
+        else{
+            int idx = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+            temp[idx] = nums[i];
+        }
+    }
+    return len;
+}
 int main()
 {
     vector<int> nums = {10,9,2,5,3,7,101,18};
